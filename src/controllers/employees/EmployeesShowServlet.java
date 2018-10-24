4230680580d31828d77a7850c4484d8263d991b0
nace <anshimaki@gmail.com>
2018-10-24 23:20:50 +0900

    Add Employee index

---
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -0,0 +1,21 @@
+package controllers.employees;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class EmployeesShowServlet
+ */
+@WebServlet("/employees/show")
+public class EmployeesShowServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesShowServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
